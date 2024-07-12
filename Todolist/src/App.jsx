8ebeeeb39422518398aaa0b82230a4418be7ceb8
User <User@WRSD-LD309-017.dut.ac.za>
2024-07-12 10:05:17 +0200

    final commit

---
+++ Todolist/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="tem"/>
+    </div>
+  </form>
+}
+
+export default App
+
